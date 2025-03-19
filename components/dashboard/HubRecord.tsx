a400718c32daeca8d9be08e0e0b67bfd87db2190
jelina <jelina@mindnetwork.xyz>
2025-03-19 11:42:16 +0800

    update

--- components/dashboard/HubRecord.tsx
+++ components/dashboard/HubRecord.tsx
@@ -1,17 +1,28 @@
 "use client";
 import { Table, TableColumnsType } from "antd";
 import Lock from "@/components/utils/Lock";
-import { useHubList } from "@/sdk";
+import { useHubGetCurrent, useHubList } from "@/sdk";
+import useAgentGetTokenIdStore from "@/store/useAgentGetTokenId";
+import useGetLearningHubId from "@/store/useGetLearningHubId";
 
 const tableColumns: TableColumnsType = [
   {
     title: "Basic Hub",
     dataIndex: "subnetName",
-    render: (value, record) => (
+    render: (value: string, record: any) => (
       <div className="flex items-center gap-[5px]">
         <img src={record.subnetLogo} alt="logo" />
         <span className="text-[14px] text-white mr-[10px]">{value}</span>
-        <Lock />
+        <div
+          className={`${
+            record.status === "Training" &&
+            Number(record.currentLearned) !== Number(record.lockupHours)
+              ? ""
+              : "hidden"
+          }`}
+        >
+          <Lock />
+        </div>
       </div>
     ),
   },
@@ -33,33 +44,27 @@ const tableColumns: TableColumnsType = [
   },
 ];
 export default function HubRecord() {
-  const { data: hubList } = useHubList();
+  const { data: hubList, loading } = useHubList();
   console.log("hubList", hubList);
-  const tableData = hubList?.filter((item: any) => ({
-    subnetId: item.id,
-    subnetName: item.name,
-    subnetLogo: "/icons/fcn.svg",
-  }));
-  // const tableData = [
-  //   {
-  //     subnetId: "1",
-  //     subnetName: "FDN",
-  //     subnetLogo: "/icons/fcn.svg",
-  //     status: "Training",
-  //     lockupHours: "72",
-  //     currentLearned: "2",
-  //     rewards: "0.3 FHE",
-  //   },
-  //   {
-  //     subnetId: "2",
-  //     subnetName: "FCN",
-  //     subnetLogo: "/icons/fcn.svg",
-  //     status: "Exit",
-  //     lockupHours: "72",
-  //     currentLearned: "72",
-  //     rewards: "0.3 FHE",
-  //   },
-  // ];
+  const agentTokenId = useAgentGetTokenIdStore((state) => state.agentTokenId);
+  const { loading: learningIdLoading } = useHubGetCurrent({
+    tokenId: agentTokenId,
+  });
+
+  const learningId = useGetLearningHubId((state) => state.learningHubId);
+  const tableData = hubList?.map((item: any) => {
+    const status = item.id === learningId ? "Training" : "Exit";
+    return {
+      subnetId: item.id,
+      subnetName: item.name,
+      subnetLogo: "/icons/fcn.svg",
+      status: learningIdLoading && learningId === 0 ? "loading..." : status,
+      lockupHours: item.lockUp,
+      currentLearned: "2",
+      rewards: "0.3 FHE",
+    };
+  });
+
   return (
     <div className="mind-table mt-[50px]">
       <div className="text-[18px] font-[800] mb-[30px]">
@@ -73,6 +78,7 @@ export default function HubRecord() {
         bordered={false}
         rowKey={"subnetId"}
         scroll={{ x: 764 }}
+        loading={loading}
       />
     </div>
   );
