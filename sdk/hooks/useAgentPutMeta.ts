b696532dca4f3f87a700c8db3f81224dd646f047
joshua <joshua@mindnetwork.xyz>
2025-03-20 22:14:04 +0800

    update hook of useAgentPutMeta

--- sdk/hooks/useAgentPutMeta.ts
+++ sdk/hooks/useAgentPutMeta.ts
@@ -3,25 +3,30 @@ import { Options } from "../types";
 import request from "../request";
 import { useAccount, useSignMessage } from "wagmi";
 import { exceptionHandler } from "../utils/exception";
+import { AGENT1_ADDRESS } from "../blockChain/address";
 
 type PostData = {
   agentId: number;
   agentName: string;
   avatar: string;
-  signature?: string;
 };
 
 export default function useAgentPutMeta(options?: Options<unknown, [PostData]>) {
-  const { address } = useAccount();
+  const { address, chainId } = useAccount();
   const { signMessageAsync } = useSignMessage();
   const result = useRequest(
     async (postData) => {
+      if (!chainId) {
+        return;
+      }
       // 签名
       const signature = await signMessageAsync({ message: `You are modifying your agent information.` });
       const res = await request.put("/user-agent", {
         ...postData,
         walletAddress: address,
+        chainId,
         signature,
+        agentAddress: AGENT1_ADDRESS[chainId],
       });
       return res;
     },
