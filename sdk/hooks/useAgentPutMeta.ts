0f5dd4f95ccb4d7585646b255604ca0d84e65ea1
joshua <joshua@mindnetwork.xyz>
2025-03-20 13:10:05 +0800

    修改元信息增加验签

--- sdk/hooks/useAgentPutMeta.ts
+++ sdk/hooks/useAgentPutMeta.ts
@@ -1,28 +1,33 @@
 import { useRequest } from "ahooks";
 import { Options } from "../types";
 import request from "../request";
-import { useAccount } from "wagmi";
+import { useAccount, useSignMessage } from "wagmi";
+import { exceptionHandler } from "../utils/exception";
 
 type PostData = {
   agentId: number;
   agentName: string;
   avatar: string;
+  signature: string;
 };
 
-export default function useAgentPutMeta(
-  options?: Options<unknown, [PostData]>
-) {
+export default function useAgentPutMeta(options?: Options<unknown, [PostData]>) {
   const { address } = useAccount();
+  const { signMessageAsync } = useSignMessage();
   const result = useRequest(
     async (postData) => {
+      // 签名
+      const signature = await signMessageAsync({ message: `You are modifying your agent information.` });
       const res = await request.put("/user-agent", {
         ...postData,
         walletAddress: address,
+        signature,
       });
       return res;
     },
     {
       manual: true,
+      onError: (err) => exceptionHandler(err),
       ...options,
     }
   );
