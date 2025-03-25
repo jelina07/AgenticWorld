4e6968993eb74bd12fcdcf82420d8f147e112a0f
jelina <jelina@mindnetwork.xyz>
2025-03-25 21:43:58 +0800

    update

--- components/dashboard/LaunchAgent.tsx
+++ components/dashboard/LaunchAgent.tsx
@@ -1,7 +1,12 @@
+import { useChainModal } from "@rainbow-me/rainbowkit";
+import { Button } from "antd";
 import Link from "next/link";
 import React from "react";
+import { useAccount } from "wagmi";
 
 export default function LaunchAgent() {
+  const { address } = useAccount();
+  const { openChainModal } = useChainModal();
   return (
     <div className="lg:w-[70%] lg:mx-auto">
       <div className="font-[800]">My Agent</div>
@@ -9,6 +14,16 @@ export default function LaunchAgent() {
         <Link href="/agentlaunch" className="btn-Link-brand-font">
           Launch Your First AI Agent
         </Link>
+      </div>
+      <div className={`mt-[20px] flex gap-[10px] ${address ? "" : "hidden"}`}>
+        <Button
+          type="primary"
+          className="button-brand-border-white-font"
+          onClick={openChainModal}
+        >
+          <img src="/icons/switch.svg" alt="switch" />
+          Switch Chain
+        </Button>
       </div>
     </div>
   );
