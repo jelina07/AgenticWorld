a32bf52fbc448a130834d1c4502f52ac247857ed
jelina <jelina@mindnetwork.xyz>
2025-03-25 18:49:57 +0800

    fix

---
+++ components/utils/created.tsx
@@ -0,0 +1,10 @@
+import LockLogo from "@/public/icons/lock.svg";
+
+export default function Created() {
+  return (
+    <div className="flex items-center px-[10px] rounded-[4px] bg-[#1D1D1D] gap-[10px] h-[24px] border border-[#484848]">
+      <LockLogo />
+      <span className="text-[12px]">Created</span>
+    </div>
+  );
+}
