f682e4a984d1af0992b307ff22abbcbc9fc53729
jelina <jelina@mindnetwork.xyz>
2025-06-02 16:45:30 +0800

    fix

--- components/worldAiHealthHub/UploadHealthData.tsx
+++ components/worldAiHealthHub/UploadHealthData.tsx
@@ -1,5 +1,26 @@
+import { Steps } from "antd";
 import React from "react";
 
 export default function UploadHealthData() {
-  return <div>UploadHealthData</div>;
+  return (
+    <div className="mt-[28px] mb-[60px] px-[28px]">
+      <Steps
+        current={1}
+        items={[
+          {
+            title: "Finished",
+          },
+          {
+            title: "In Progress",
+          },
+          {
+            title: "Waiting",
+          },
+        ]}
+      />
+      <div>
+        <div>Health Symptoms Selection</div>
+      </div>
+    </div>
+  );
 }
